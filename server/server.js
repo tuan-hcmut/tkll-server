b4286f90dea24a82a3c418da8ff430197029c5f8
tuan <tuan01677239677@gmail.com>
2022-11-02 20:04:59 +0700

    change

--- server/server.js
+++ server/server.js
@@ -18,20 +18,47 @@ mongoose
     console.log("DB connected!!");
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => {
   console.log(`Listen on port 127.0.0.1:${port}`);
 });
 
-const client = mqtt.connect("mqtt://test.mosquitto.org", {
-  host: "energy-blockchain.cloud.shiftr.io",
-  port: 1883,
-});
+const client = mqtt.connect(
+  `mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`,
+  {
+    clientId: process.env.MQTT_CLIENTID,
+    clean: true,
+    connectTimeout: 4000,
+    username: process.env.MQTT_USERNAME.toString(),
+    password: process.env.MQTT_PASSWORD.toString(),
+    reconnectPeriod: 1000,
+  }
+);
 
 client.on("connect", () => {
-  console.log("sdfsdfsd");
+  console.log("connected");
+
+  client.subscribe(process.env.MQTT_TOPIC.toString(), () => {
+    console.log(`Subscribe to topic '${process.env.MQTT_TOPIC.toString()}'`);
+  });
+
+  client.publish(
+    process.env.MQTT_TOPIC.toString(),
+    "nodejs mqtt test",
+    { qos: 0, retain: false },
+    (error) => {
+      if (error) {
+        console.error(error);
+      }
+    }
+  );
 });
+
+client.on("message", (topic, payload) => {
+  console.log("Received Message:", topic, payload.toString());
+});
+
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 initSockets(io);
--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -3,6 +3,9 @@ const initSockets = (io) => {
   io.on("connection", (socket) => {
     console.log("Socket connected!!");
 
+    socket.on("new-connect", () => {
+      console.log("hello!!!");
+    });
     socket.on("disconnect", () => {});
   });
 };
