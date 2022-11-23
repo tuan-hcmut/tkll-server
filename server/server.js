e403a689ccba7adb8ba19835f7966b17d39656e4
tuan <tuan01677239677@gmail.com>
2022-11-23 20:18:57 +0700

    change

--- server/server.js
+++ server/server.js
@@ -4,6 +4,15 @@ const initSockets = require("./src/sockets");
 const mqtt = require("mqtt");
 dotenv.config({ path: "./config.env" });
 
+let customer = [];
+let seller = [];
+let currentUser = {
+  username: "",
+  role: "",
+  energyRemaining: 0,
+  ethRemaining: 0,
+};
+
 const app = require("./app");
 
 const DB = process.env.DATABASE;
@@ -24,41 +33,74 @@ const server = app.listen(port, () => {
   console.log(`Listen on port 127.0.0.1:${port}`);
 });
 
-const client = mqtt.connect(
-  `mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`,
-  {
-    clientId: process.env.MQTT_CLIENTID,
-    clean: true,
-    connectTimeout: 4000,
-    username: process.env.MQTT_USERNAME.toString(),
-    password: process.env.MQTT_PASSWORD.toString(),
-    reconnectPeriod: 1000,
-  }
-);
-
-client.on("connect", () => {
-  console.log("connected");
-
-  client.subscribe(process.env.MQTT_TOPIC.toString(), () => {
-    console.log(`Subscribe to topic '${process.env.MQTT_TOPIC.toString()}'`);
-  });
-
-  client.publish(
-    process.env.MQTT_TOPIC.toString(),
-    "nodejs mqtt test",
-    { qos: 0, retain: false },
-    (error) => {
-      if (error) {
-        console.error(error);
-      }
-    }
-  );
+const client = mqtt.connect(`mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`, {
+  clientId: process.env.MQTT_CLIENTID,
+  clean: true,
+  connectTimeout: 4000,
+  username: process.env.MQTT_USERNAME.toString(),
+  password: process.env.MQTT_PASSWORD.toString(),
+  reconnectPeriod: 1000,
 });
 
-client.on("message", (topic, payload) => {
-  console.log("Received Message:", topic, payload.toString());
-});
+//////// socket /////////////////
 
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
-initSockets(io);
+io.on("connection", (socket) => {
+  socket.on("data-register", (data) => {
+    ///// data : {username: String, role: String, energyRemaining: Number, ethRemaining: Number}   //////
+    if (customer.length === 0) {
+      customer.push(data);
+      currentUser = data;
+    }
+    for (let i = 0; i < customer.length; i++) {
+      if (customer[i].username === data.username) {
+        currentUser = customer[i];
+        socket.emit("error", { message: "username already exist!!", data: customer[i] });
+        break;
+      } else {
+        customer.push(data);
+        currentUser = data;
+        socket.emit("success", { message: "Add success!!!", data: data });
+        break;
+      }
+    }
+
+    console.log(customer, currentUser);
+  });
+  socket.on("disconnect", () => {});
+
+  /////////////// connect to broker //////////////////
+
+  client.on("connect", () => {
+    client.subscribe(process.env.MQTT_TOPIC.toString(), () => {
+      console.log(`Subscribe to topic '${process.env.MQTT_TOPIC.toString()}'`);
+    });
+  });
+
+  client.on("message", (topic, payload) => {
+    // console.log("Received Message:", topic, payload.toString());
+    // esp send num of energy remaining (each time eps send, num of energy will be reduce)
+    if (topic.toString() === "mqtt/nodejs") {
+      const energyRemaining = currentUser.energyRemaining - 1;
+      currentUser.energyRemaining = energyRemaining;
+      if (energyRemaining <= 0) {
+        client.publish(process.env.MQTT_TOPIC.toString(), "Done", { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+        socket.emit("energy-gone", "Energy is 0!!!");
+      } else {
+        client.publish(process.env.MQTT_TOPIC.toString(), energyRemaining.toString(), { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+        socket.emit("energy-remaining", energyRemaining);
+      }
+    }
+  });
+});
+
+// initSockets(io);
--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -1,10 +1,21 @@
 const initSockets = (io) => {
-  let clients = {};
-  io.on("connection", (socket) => {
-    console.log("Socket connected!!");
+  let customer = [];
+  let seller = [];
 
-    socket.on("new-connect", () => {
-      console.log("hello!!!");
+  io.on("connection", (socket) => {
+    console.log(customer);
+    socket.on("data-register", (data) => {
+      if (customer.length === 0) customer.push(data);
+      for (let i = 0; i < customer.length; i++) {
+        if (customer[i].username === data.username) {
+          socket.emit("error", { message: "username already exist!!", data: customer[i] });
+          break;
+        } else {
+          customer.push(data);
+          socket.emit("success", { message: "Add success!!!", data: data });
+          break;
+        }
+      }
     });
     socket.on("disconnect", () => {});
   });
