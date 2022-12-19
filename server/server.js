cbc52d6eb258a044e6cbd366cd963b1313f1e868
tuan <tuan01677239677@gmail.com>
2022-12-19 21:00:42 +0700

    change

--- server/server.js
+++ server/server.js
@@ -42,67 +42,116 @@ const client = mqtt.connect(`mqtt://${process.env.MQTT_SERVER}:${process.env.MQT
   reconnectPeriod: 1000,
 });
 
-const handleSave = (type, data) => {
-  if (type.length === 0) {
-    type.push(data);
-    currentUser = data;
-  }
-  for (let i = 0; i < type.length; i++) {
-    if (type[i].username === data.username) {
-      currentUser = type[i];
-      socket.emit("error", { message: "username already exist!!", data: type[i] });
-      break;
-    } else {
-      type.push(data);
-      currentUser = data;
-      socket.emit("success", { message: "Add success!!!", data: data });
-      break;
-    }
-  }
-  // console.log(customer, currentUser);
-};
 //////// socket /////////////////
 
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
+  console.log("socket connected");
+  const handleSave = (type, data) => {
+    data.energyRemaining = 20;
+    if (type.length === 0) {
+      type.push(data);
+      currentUser = data;
+    }
+    for (let i = 0; i < type.length; i++) {
+      if (type[i].username === data.username) {
+        currentUser = type[i];
+        socket.emit("error", { message: "username already exist!!", data: type[i] });
+        break;
+      } else {
+        type.push(data);
+        currentUser = data;
+        socket.emit("success", { message: "Add success!!!", data: data });
+        break;
+      }
+    }
+    // console.log(customer, currentUser);
+  };
   socket.on("data-register", (data) => {
     ///// data : {username: String, role: String, energyRemaining: Number, ethRemaining: Number}   //////
     data.type === "buyer" ? handleSave(customer, data) : handleSave(seller, data);
+    socket.emit("EnergyRemaining", currentUser.energyRemaining);
+    console.log(currentUser);
   });
   socket.on("disconnect", () => {});
 
   /////////////// connect to broker //////////////////
 
   client.on("connect", () => {
-    client.subscribe(process.env.MQTT_TOPIC.toString(), () => {
-      console.log(`Subscribe to topic '${process.env.MQTT_TOPIC.toString()}'`);
+    console.log("connect to mqtt broker");
+    client.subscribe(process.env.MQTT_TOPIC1.toString(), () => {
+      console.log(`Subscribe to topic ${process.env.MQTT_TOPIC1?.toString()}`);
+    });
+    client.subscribe(process.env.MQTT_TOPIC2.toString(), () => {
+      console.log(`Subscribe to topic ${process.env.MQTT_TOPIC2?.toString()}`);
+    });
+    client.subscribe(process.env.MQTT_TOPIC3.toString(), () => {
+      console.log(`Subscribe to topic ${process.env.MQTT_TOPIC3?.toString()}`);
     });
   });
-
-  client.on("message", (topic, payload) => {
-    // console.log("Received Message:", topic, payload.toString());
-    // esp send num of energy remaining (each time eps send, num of energy will be reduce)
-    if (topic.toString() === "mqtt/nodejs") {
-      const energyRemaining = currentUser.energyRemaining - 1;
-      currentUser.energyRemaining = energyRemaining;
-      if (energyRemaining <= 0) {
-        client.publish(process.env.MQTT_TOPIC.toString(), "Done", { qos: 0, retain: false }, (error) => {
-          if (error) {
-            console.log(error);
-          }
-        });
-        socket.emit("energy-gone", "Energy is 0!!!");
-      } else {
-        client.publish(process.env.MQTT_TOPIC.toString(), energyRemaining.toString(), { qos: 0, retain: false }, (error) => {
-          if (error) {
-            console.log(error);
-          }
-        });
-        socket.emit("energy-remaining", energyRemaining);
+  const handleTransferData = () => {
+    console.log("start again");
+    client.on("message", (topic, payload) => {
+      console.log("Received Message:", topic, payload.toString());
+      // esp send num of energy remaining (each time eps send, num of energy will be reduce)
+      if (topic.toString() === "mqtt/energyUsed") {
+        const energyRemaining = currentUser.energyRemaining - parseInt(payload.toString("utf8"));
+        console.log("energy used: " + parseInt(payload.toString("utf8")));
+        //const energyRemaining = 10;
+        currentUser.energyRemaining = energyRemaining;
+        if (currentUser.energyRemaining < 0) {
+          currentUser.energyRemaining = 0;
+        }
+        socket.emit("EnergyRemaining", currentUser.energyRemaining);
+        console.log(currentUser);
       }
-    }
+      if (topic.toString() === "mqtt/connection") {
+        console.log("device connected");
+        if (currentUser.energyRemaining <= 0) {
+          currentUser.energyRemaining = 0;
+          socket.emit("out-of-energy", "");
+          console.log("no energy left");
+          client.publish("mqtt/remainingEnergy", "done", { qos: 0, retain: false }, (error) => {
+            if (error) {
+              console.log(error);
+            }
+            return;
+          });
+        } else {
+          console.log("start connected, eneryremaning: " + currentUser.energyRemaining);
+          client.publish("mqtt/remainingEnergy", "keep", { qos: 0, retain: false }, (error) => {
+            if (error) {
+              console.log(error);
+            }
+          });
+        }
+      }
+      if (currentUser.energyRemaining <= 0) {
+        currentUser.energyRemaining = 0;
+        socket.emit("out-of-energy", "");
+        console.log("no energy left");
+        client.publish("mqtt/remainingEnergy", "done", { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.log(error);
+          }
+          return;
+        });
+      } else {
+        client.publish("mqtt/remainingEnergy", "keep", { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    });
+  };
+
+  handleTransferData();
+
+  socket.on("buy-more-energy", (data) => {
+    currentUser.energyRemaining = currentUser.energyRemaining + data;
+    handleTransferData();
+    console.log(currentUser.energyRemaining);
   });
 });
-
-// initSockets(io);
